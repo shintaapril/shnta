bc0eba82d44dcdea844cdf1e0c8a8d745566e69b
shnta <shntaaprilia9@gmail.comgit config --global user.name shintaapril>
2025-04-30 15:13:58 +0900

    php

---
+++ kalkulator/kalkulator.php
@@ -0,0 +1,36 @@
+<?php
+function kalkulator($x, $y, $operasi) {
+    if ($operasi == '+') {
+        return $x + $y;
+    } else if ($operasi == '-') {
+        return $x - $y;
+    } else if ($operasi == '*') {
+        return $x * $y;
+    } else if ($operasi == '/') {
+        if ($y == 0) {
+            return "Tidak bisa membagi dengan nol!";
+        }
+        return $x / $y;
+    } else {
+        return "Operasi tidak dikenali";
+    }
+}
+
+// Array perhitungan dengan keterangan operasi
+$perhitungan = [
+    ['Penjumlahan', 10, 15, '+'],
+    ['Pengurangan', 11, 8, '-'],
+    ['Perkalian', 8, 4, '*'],
+    ['Pembagian', 8, 2, '/'],
+    ['Pembagian Error', 5, 0, '/']
+];
+
+foreach ($perhitungan as $data) {
+    $nama = $data[0];
+    $x = $data[1];
+    $y = $data[2];
+    $op = $data[3];
+    $hasil = kalkulator($x, $y, $op);
+    echo "$nama: $x $op $y = $hasil<br>";
+}
+?>
