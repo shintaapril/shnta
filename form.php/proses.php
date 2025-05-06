c3830555818fb4472d85439542923206f48050f9
shnta <shntaaprilia9@gmail.comgit config --global user.name shintaapril>
2025-05-06 21:14:00 +0900

    shinta

--- form.php/proses.php
+++ form.php/proses.php
@@ -1,27 +1,8 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama_depan = $_POST['nama_depan'];
-    $nama_belakang = $_POST['nama_belakang'];
-    $tingkat = $_POST['tingkat'];
-    $jenis_kelamin = $_POST['jenis_kelamin'];
-    $hobi = isset($_POST['hobi']) ? $_POST['hobi'] : [];
+$nama = $_POST['nama_depan'] . ' ' . $_POST['nama_belakang'];
+$tingkat = $_POST['tingkat'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$hobi = implode(", ", $_POST['hobi']);
 
-    echo "<h2>Data yang Anda Kirim:</h2>";
-    echo "Nama Lengkap: " . htmlspecialchars($nama_depan) . " " . htmlspecialchars($nama_belakang) . "<br>";
-    echo "Tingkat: " . htmlspecialchars($tingkat) . "<br>";
-    echo "Jenis Kelamin: " . htmlspecialchars($jenis_kelamin) . "<br>";
-    echo "Hobi: <br>";
-
-    if (!empty($hobi)) {
-        echo "<ul>";
-        foreach ($hobi as $item) {
-            echo "<li>" . htmlspecialchars($item) . "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "Tidak ada hobi yang dipilih.";
-    }
-} else {
-    echo "Tidak ada data yang dikirim.";
-}
+echo "Nama saya adalah $nama. Saya sekolah tingkat $tingkat. Jenis kelamin saya $jenis_kelamin. Dan hobi saya $hobi.";
 ?>
